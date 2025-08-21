5a9f921c288acd16b32c2c696badc356245bb33f
Andrew Davis <addavis@gmail.com>
2025-08-21 15:33:28 -0400

    - new docsh format

--- physpath.sh
+++ physpath.sh
@@ -3,59 +3,62 @@
 import_func phys_dirpath \
     || return
 
+: """Print physical path, after resolving symlinks
+
+    Usage: physpath <path>
+
+    The path argument may be a symbolic link, file, or directory. This function
+    prints the absolute path of the target with any symlinks or relative references
+    ('.' and '..') resolved.
+
+    Unlike some other similar tools such as GNU 'readlink -f' or 'realpath', the
+    path argument must exist on the filesystem. Since physpath is written as a
+    shell script and relies on cross-platform tools, it runs quickly and can be
+    used on Linux and macOS systems.
+
+    Notes
+
+      - Works on GNU/Linux and BSD-like systems, including macOS. It relies on the
+        \`file\` command to resolve symlinks in the basename, which generally has a
+        consistent interface across systems, unlike the \`stat\`, \`ls\`, and
+        \`readlink\` commands.
+
+      - Symlinks in the path are resolved after instances of '..', using the shell
+        built-in command 'cd -L'.
+
+        For more on the '-P' and '-L' options to cd and pwd, refer to the
+        phys_dirpath function documentation. The -L or -P option may be supplied on
+        the command-line of this function, to be passed along to the phys_dirpath
+        command when resolving directory paths.
+
+      - An earlier version of this function is posted as an [answer](https://apple.stackexchange.com/a/444039/61160),
+        with testing examples.
+
+    Alternatives
+
+      - For some applications, it's possible to write safe and effective one-liners to
+        resolve symbolic links. E.g., if you know that you're resolving a directory
+        path, you can use a one-liner similar to:
+
+        dir=\$( cd -- \"\$path\" &> /dev/null  && pwd -P )
+
+      - A common use case is the need to obtain the absolute path of a script file while
+        it's running. Some would advise to use a line like the one above, using the
+        output of 'dirname \${BASH_SOURCE[0]}' for the path. However, this fails in the
+        common case that the basename of the path is a symlink. The subtleties that can
+        happen when resolving symlinks is the reason this function exists.
+
+      - A cross-platform alternative is to use a Python command to resolve symlinks in a
+        file path. E.g.:
+
+        python3 -c \"import os.path; print(os.path.realpath('\${BASH_SOURCE[0]}'))\"
+"""
+
 physpath() {
 
     # docs
-    if [[ $# -eq 0  || $1 == @(-h|--help) ]]
-    then
-        : """Print physical path, after resolving symlinks
-
-        Usage: physpath <path>
-
-        The path argument may be a symbolic link, file, or directory. This function
-        prints the absolute path of the target with any symlinks or relative references
-        ('.' and '..') resolved.
-
-        Unlike some other similar tools such as GNU 'readlink -f' or 'realpath', the
-        path argument must exist on the filesystem. Since physpath is written as a
-        shell script and relies on cross-platform tools, it runs quickly and can be
-        used on Linux and macOS systems.
-
-        Notes
-
-          - Works on GNU/Linux and BSD-like systems, including macOS. It relies on the
-            \`file\` command to resolve symlinks in the basename, which generally has a
-            consistent interface across systems, unlike the \`stat\`, \`ls\`, and
-            \`readlink\` commands.
-
-          - Symlinks in the path are resolved after instances of '..', using the shell
-            built-in command 'cd -L'.
-
-            For more on the '-P' and '-L' options to cd and pwd, refer to the
-            phys_dirpath function documentation. The -L or -P option may be supplied on
-            the command-line of this function, to be passed along to the phys_dirpath
-            command when resolving directory paths.
-
-          - An earlier version of this function is posted as an [answer](https://apple.stackexchange.com/a/444039/61160),
-            with testing examples.
-
-        Alternatives
-
-          - For some applications, it's possible to write safe and effective one-liners to
-            resolve symbolic links. E.g., if you know that you're resolving a directory
-            path, you can use a one-liner similar to:
-
-            dir=\$( cd -- \"\$path\" &> /dev/null  && pwd -P )
-
-          - A common use case is the need to obtain the absolute path of a script file while
-            it's running. Some would advise to use a line like the one above, using the
-            output of 'dirname \${BASH_SOURCE[0]}' for the path. However, this fails in the
-            common case that the basename of the path is a symlink. The subtleties that can
-            happen when resolving symlinks is the reason this function exists.
-        """
-        docsh -TD
-        return
-    fi
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] \
+        && { docsh -TD; return; }
 
     # clean up
     # - NB, I would like to put _rslv_link in a separate file and use 'import_func -l'
@@ -67,6 +70,7 @@ physpath() {
     ' RETURN
 
     _rslv_link() {
+
         # resolve symlink target with 'file' command
         # - prints e.g. 'symbolic link to ...', 'cannot open ...', 'directory', 'empty',
         #   'ASCII text', etc.
